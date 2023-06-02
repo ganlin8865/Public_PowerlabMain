128235c2730b5e1c5fac784b579f151badb07033
lin gan <LuGanlin8865@outlook.com>
2023-06-02 18:02:19 +0800

    Test

---
+++ PowerlabMain-PowerlabSpeech.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace PowerlabMain
+{
+    partial class PowerlabMain_PowerlabSpeech
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new System.Windows.Forms.Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new System.Drawing.Point(669, 259);
+            button1.Name = "button1";
+            button1.Size = new System.Drawing.Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // PowerlabMain_PowerlabSpeech
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(984, 561);
+            Controls.Add(button1);
+            Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            Name = "PowerlabMain_PowerlabSpeech";
+            Text = "PowerlabSpeech语音助手";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ PowerlabMain-PowerlabSpeech.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+
+namespace PowerlabMain
+{
+    public partial class PowerlabMain_PowerlabSpeech : Form
+    {
+        public PowerlabMain_PowerlabSpeech()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer();
+            speechSynthesizer.Speak("Hi!");
+        }
+    }
+}
